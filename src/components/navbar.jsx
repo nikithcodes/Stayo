f4181fd25204e4bf11d1da42e8410700a82e879f
Nikith Gowda BS <nikithg36@gmail.com>
2025-07-23 09:33:40 +0530

    final

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -69,7 +69,7 @@ const Navbar = () => {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/hotels">Hotels</Link></li>
             <li><Link to="/about">About</Link></li>
-            <li><a href="#contact">Contact</a></li>
+            
           </ul>
 
           <div className="auth-buttons">
